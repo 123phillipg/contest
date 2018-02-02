92224de306d847d6d292eaff422615333b1ad7e4
Phillip Gonzalez <123phillipg@gmail.com>
2018-02-01 16:35:43 -0800

    added the other two credentials

--- contest.py
+++ contest.py
@@ -1,6 +1,10 @@
+#Acquire twitter credentials from the text file that isn't stored on github
 credentials = open("credentials.txt","r")
 consumerKey = credentials.readline()
 consumerSecret = credentials.readline()
+accessToken = credentials.readline()
+accessTokenSecret = credentials.readline()
 
-print "consumerKey= ", consumerKey
-print "consumerSecret= ", consumerSecret
+
+import twitter
+api = twitter.Api(consumer_key = "\'"+consumerKey+"\'", consumer_secret = "\'" + consumerSecret + "\'", access_token_key = "\'" + accessToken + "\'", access_token_secret = "\'" + accessTokenSecret + "\'")
