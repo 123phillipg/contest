34e3b7d51f34e330f91e553e3a2219031b9dc5da
Phillip Gonzalez <123phillipg@gmail.com>
2018-02-01 16:21:03 -0800

    Acquired the keys from the hidden text file

--- contest.py
+++ contest.py
@@ -1,1 +1,6 @@
+credentials = open("credentials.txt","r")
+consumerKey = credentials.readline()
+consumerSecret = credentials.readline()
 
+print "consumerKey= ", consumerKey
+print "consumerSecret= ", consumerSecret
