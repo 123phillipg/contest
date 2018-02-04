7bf07f6c466b432bc7f2f34520bec9a424cea82c
Phillip Gonzalez <123phillipg@gmail.com>
2018-02-03 18:25:10 -0800

    Added viewing a bunch of searched messages.

--- contest.py
+++ contest.py
@@ -1,15 +1,18 @@
 #Acquire twitter credentials from the text file that isn't stored on github
 credentials = open("credentials.txt","r")
-consumerKey = credentials.readline().rstrip()
-consumerSecret = credentials.readline().rstrip()
-accessToken = credentials.readline().rstrip()
-accessTokenSecret = credentials.readline().rstrip()
+CONSUMER_KEY = credentials.readline().rstrip()
+CONSUMER_SECRET = credentials.readline().rstrip()
+ACCESS_TOKEN = credentials.readline().rstrip()
+ACCESS_TOKEN_SECRET = credentials.readline().rstrip()
 
 import tweepy
 
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
+search_results = api.search(q = "RT to win", count = 100)
 
+for tweet in search_results:
+    print tweet.text
